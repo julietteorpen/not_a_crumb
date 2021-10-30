15abd9bb40f01f349950de15cd99494bf5cdd2be
neprojo <53294140+neprojo@users.noreply.github.com>
2021-10-30 15:55:28 +0200

    continued work on cake visualiser and related JS

---
+++ script.js
@@ -0,0 +1,25 @@
+let form = document.querySelector("form");
+let tier = document.querySelector("#tier");
+let flavour1 = document.querySelector("#flavour1");
+let flavour2 = document.querySelector("#flavour2");
+let flavour3 = document.querySelector("#flavour3");
+let flavour4 = document.querySelector("#flavour4");
+let flavour5 = document.querySelector("#flavour5");
+
+//create empty variable to hold total value
+
+//check form to see flavour selected - e.g. vanilla selected
+//if vanilla select, loop through vanilla array images
+
+//look at number of tiers. Times total by no. of tiers entered
+
+//look at decoration and add value to total - none = 0, sprinkles = 1,
+
+let decorations = [];
+let vanillaCake = [];
+let chocCake = [];
+
+function visualise() {
+  let total = 0;
+  console.log(total);
+}
