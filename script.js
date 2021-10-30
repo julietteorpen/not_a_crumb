4a2724734b481b0eec89698c092fbf3089cbe25d
neprojo <53294140+neprojo@users.noreply.github.com>
2021-10-30 18:00:05 +0200

    worked on cake visualiser

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let form = document.querySelector("form");
+let cakeForm = document.querySelector("form");
 let tier = document.querySelector("#tier");
 let flavour1 = document.querySelector("#flavour1");
 let flavour2 = document.querySelector("#flavour2");
@@ -15,11 +15,29 @@ let flavour5 = document.querySelector("#flavour5");
 
 //look at decoration and add value to total - none = 0, sprinkles = 1,
 
-let decorations = [];
+let decorations = Array.from(document.querySelectorAll("input[type=checkbox]"));
 let vanillaCake = [];
 let chocCake = [];
 
-function visualise() {
+function visualise(event) {
+  event.preventDefault();
   let total = 0;
   console.log(total);
+  const value = event.target.elements.tier.value;
+  total = parseInt(total) + parseInt(value);
+  console.log(total);
+  console.log(decorations);
+  for (let i = 0; i < decorations.length; i++) {
+    if (decorations[i].checked === true) {
+      total++;
+    }
+    console.log(total);
+  }
 }
+cakeForm.addEventListener("submit", visualise);
+
+// const parent = document.getElementById("parent");
+// let child_nodes = parent.childNodes;
+// console.log(child_nodes.length); // let's assume "2"
+// parent.appendChild(document.createElement("div"));
+// console.log(child_nodes.length); // outputs "3"
