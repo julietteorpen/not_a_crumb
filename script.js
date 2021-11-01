2263ecce899a2be7f8552c97b0a4aeb2384c2e1c
neprojo <53294140+neprojo@users.noreply.github.com>
2021-11-01 21:41:24 +0000

    started adding alternate cake images and working on JS

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ cakeForm.addEventListener("submit", e => {
   let minMaxTierVal = tier.value;
   if (minMaxTierVal === 0) {
     alert("Cakes must be at least one tier!");
-  } else if (minMaxTierVal >= 4) {
+  } else if (minMaxTierVal >= 5) {
     alert("Cakes can be a maximum of 4 tiers to stop any caketastrophes!");
   }
 });
@@ -46,25 +46,32 @@ function visualise(event) {
 }
 cakeForm.addEventListener("submit", visualise);
 cakeForm.addEventListener("submit", changeVanillaTier);
+cakeForm.addEventListener("submit", cakeFlavourDecoration);
 
 //select the contents of all the cake images
 const vanillaImages = Array.from(document.querySelectorAll(".vanilla-image"));
+const coffeeImages = Array.from(document.querySelectorAll(".coffee-image"));
 console.log(vanillaImages);
 
 function changeVanillaTier(event) {
   //access value as an integer through a variable
-  const value = event.target.elements.tier.value;
+  const tierValue = event.target.elements.tier.value;
   //hide all userBios
   for (let i = 0; i < vanillaImages.length; i++) {
     vanillaImages[i].classList.add("hidden");
-    if (parseInt(value) === 1) {
+    if (parseInt(tierValue) === 1) {
       vanillaImages[0].classList.remove("hidden");
-    } else if (parseInt(value) === 2) {
+    } else if (parseInt(tierValue) === 2) {
       vanillaImages[1].classList.remove("hidden");
-    } else if (parseInt(value) === 3) {
+    } else if (parseInt(tierValue) === 3) {
       vanillaImages[2].classList.remove("hidden");
-    } else if (parseInt(value) === 4) {
+    } else if (parseInt(tierValue) === 4) {
       vanillaImages[3].classList.remove("hidden");
     }
   }
+}
+
+//a funciton to detect which decoration and flavour have been selected
+function cakeFlavourDecoration(event) {
+  event.preventDefault();
 }
