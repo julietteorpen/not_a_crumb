0849255c66e350e0bb91e1ba8350a35cd3ce921f
neprojo <53294140+neprojo@users.noreply.github.com>
2022-01-04 16:43:13 +0000

    reviewed js

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ let flavour2 = document.querySelector("#flavour2");
 let flavour3 = document.querySelector("#flavour3");
 let flavour4 = document.querySelector("#flavour4");
 let flavour5 = document.querySelector("#flavour5");
-let calculateBtn = document.querySelector("#calculate-btn");
+let calculateBtn = document.getElementById("#calculate-btn");
 
 /* Cake images */
-const vanillaImages = Array.from(document.querySelectorAll(".vanilla-image"));
+let vanillaImages = Array.from(document.querySelectorAll(".vanilla-image"));
 console.log(vanillaImages);
+let coffeeImages = Array.from(document.querySelectorAll(".coffee-images"));
+console.log(coffeeImages);
 let decorations = Array.from(document.querySelectorAll("input[type=checkbox]"));
+console.log("decorations array", decorations);
 let vanillaCake = [];
 let chocCake = [];
 
@@ -30,9 +33,33 @@ let inspoInput = document.querySelector("#flavour-inspo");
 let submitInspo = document.querySelector("#submit-inspo");
 const guardianKey = "ffb6c45e-9bcc-4828-b865-e4f13ac02107";
 let inspoResult = document.querySelector(".fetch-inspiration");
+
+/*----------------------------------------------------*\
+  EVENT LISTENERS
+\*----------------------------------------------------*/
+
+//cake visualiser
+//calculateBtn.addEventListener("submit", calculatePrice);
+//calculateBtn.addEventListener("click", changeVanillaTier);
+
+//flavour inspiration
+//flavourInspo.addEventListener("submit", fetchRecipe);
+//fetch inspiration
+submitInspo.addEventListener("click", fetchRecipe);
+
+//CAKE FORM MAX TIERS WARNING
+calculateBtn.addEventListener("submit", maxTiers);
+
+function maxTiers(e) {
+  e.preventDefault();
+  let minMaxTierVal = tier.value;
+  if (minMaxTierVal >= 4) {
+    alert("Cakes must be a maximum of 4 tiers to stop any caketastrophes!");
+  }
+}
+
 //Fetch and update DOM with random recipe title and link
 //Function to fetch random recipe from the Guardian
-
 function fetchRecipe(event) {
   event.preventDefault();
   let url = "";
@@ -66,7 +93,7 @@ function fetchRecipe(event) {
 //look at number of tiers. Times total by no. of tiers entered
 //look at decoration and add value to total - none = 0, sprinkles = 1,
 
-function visualise(event) {
+function calculatePrice(event) {
   event.preventDefault();
   let total = 0;
   console.log(total);
@@ -83,43 +110,11 @@ function visualise(event) {
 }
 
 //select the contents of all the cake images
-function changeVanillaTier(event) {
-  event.preventDefault();
-  //access value as an integer through a variable
-  const value = event.target.elements.tier.value;
-  //hide all userBios
-  for (let i = 0; i < vanillaImages.length; i++) {
-    vanillaImages[i].classList.add("hidden");
-    if (parseInt(value) === 1) {
-      vanillaImages[0].classList.remove("hidden");
-    } else if (parseInt(value) === 2) {
-      vanillaImages[1].classList.remove("hidden");
-    } else if (parseInt(value) === 3) {
-      vanillaImages[2].classList.remove("hidden");
-    } else if (parseInt(value) === 4) {
-      vanillaImages[3].classList.remove("hidden");
-    }
-  }
-}
-
-/*----------------------------------------------------*\
-  EVENT LISTENERS
-\*----------------------------------------------------*/
-
-//cake visualiser
-calculateBtn.addEventListener("submit", visualise);
-calculateBtn.addEventListener("submit", changeVanillaTier);
-
-//flavour inspiration
-//flavourInspo.addEventListener("submit", fetchRecipe);
-//fetch inspiration
-submitInspo.addEventListener("click", fetchRecipe);
-
-//CAKE FORM MAX TIERS WARNING
-calculateBtn.addEventListener("click", e => {
-  e.preventDefault();
-  let minMaxTierVal = tier.value;
-  if (minMaxTierVal >= 4) {
-    alert("Cakes must be a maximum of 4 tiers to stop any caketastrophes!");
-  }
-});
+// function changeVanillaTier(event) {
+//   event.preventDefault();
+//   //access value as an integer through a variable
+//   const value = event.target.elements.tier.value;
+//   const flavour = event.target.elements.flavour;
+//   console.log(flavour, value)`<img src="images/visualiser/${flavour}/${flavour +
+//     value}.jpg" class="vanilla-image one-tier" alt="An image of a 1 tier vanilla buttercream cake with candles is shown">`;
+// }
